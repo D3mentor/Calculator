038efd1ad426ab78cefebea344fe00489e2dd58a
D3mentor <127333615+D3mentor@users.noreply.github.com>
2023-06-21 14:44:40 +0300

    Add files via upload

---
+++ Калькулятор.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a = 0.0;
+    float b = 0.0;
+    float r = 0.0;
+    char o;
+
+    cout << "Консольный калькулятор" << endl << endl;
+    cout << "Пожалуйста, введите операцию для выполнения."<< endl <<"Формат: a + b | a - b | a * b | a / b"  << endl;
+
+    Calculator cal;
+    while (true)
+    {
+        cin >> a >> o >> b;
+        r = cal.Calculate(a, b, o);
+        cout << "Результат: " << r << endl;
+    }
+
+    return 0;
+}
